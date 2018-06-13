4632fb26db8238a716e835336bf313fbd3a19672
theguyfromtv <goldfingerorama@gmail.com>
2018-06-13 17:13:27 -0400

    fixing image issue

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./FriendCard.css";
-const image = require(props.image)
 
 
 const FriendCard = props => (
   <div className="card">
     <div className="img-container">
-      <img className="card-img" alt={"text"} src={image} onClick={() => props.gameFunction(props.id)}/>
+      <img className="card-img" alt={"text"} src={props.image} onClick={() => props.gameFunction(props.id)}/>
     </div>
   </div>
 );
